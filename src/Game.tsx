4f14602b138300aa44375b9dcf0756230ce46af0
kwarchalowski <karol@warchalowski.pl>
2022-10-06 14:08:23 +0200

    Basic board styled

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
+const INITIAL_GAME_STATE = ["X","X","O","","","","","","","",""]
+
 function Game() {
+    const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
     return ( 
         <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-blue-500 to-cyan-500">
             <h1 className="text-center text-5xl mb-4 font-display text-white">
                 Tic Tac Toe Game
             </h1>
             <div>
-                <div>Board Goes Here</div>
+                <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+                    {INITIAL_GAME_STATE.map((player, index) => (
+                        <div key={index} className="h-32 border-solid border-4 border-slate-200 font-display text-6xl text-center flex justify-center items-center cursor-pointer">{player}</div>
+                    ))}
+                </div>
                 <div>Scores Goes Here</div>
             </div>
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
-import "@fontsource/fira-code";
-
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
