942e98527083459066a427e1ff02017dc2ef3b7a
kwarchalowski <karol@warchalowski.pl>
2022-10-10 10:09:17 +0200

    Local storage

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,32 +2,55 @@ import { useEffect, useState } from "react";
 
 import Square from "./Square";
 
+type Scores = {
+    [key: string]: number
+}
+
 const INITIAL_GAME_STATE = ["","","","","","","","",""]
+const INITIAL_SCORES: Scores = { X: 0, O: 0};
+const WINNING_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 function Game() {
     const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
     const [currentPlayer, setCurrentPlayer] = useState("X");
-
-    const WINNING_COMBOS = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+    const [scores, setScores] = useState(INITIAL_SCORES);
 
     useEffect(() => {
+
+        const storedScores = localStorage.getItem("scores");
+        if (storedScores) {
+            setScores(JSON.parse(storedScores));
+        }
+    }, [])
+
+    useEffect(() => {
+        if (gameState === INITIAL_GAME_STATE) {
+            return;
+        }
+        
         checkForWinner();
-        changePlayer();
     }, [gameState]);
 
     const resetBoard = () => setGameState(INITIAL_GAME_STATE);
 
     const handleWin = () => {
         window.alert(`Congrats player ${currentPlayer}! You are the winner!`);
+
+        const newPlayerScore = scores[currentPlayer] + 1;
+        const newScores = {...scores};
+        newScores[currentPlayer] = newPlayerScore;
+        setScores(newScores);
+        localStorage.setItem("scores", JSON.stringify(newScores));
+
         resetBoard();
     }
 
@@ -35,7 +58,6 @@ function Game() {
         window.alert("The game ended in a draw");
         resetBoard();
     }
-
 
     const checkForWinner = () => {
         let roundWon = false;
@@ -55,16 +77,15 @@ function Game() {
                 roundWon = true;
                 break;
             }
-
         }
 
         if (roundWon) {
-            setTimeout(() => handleWin(), 150);
+            setTimeout(() => handleWin(), 10);
             return;
         }
 
         if (!gameState.includes("")) {
-            setTimeout(() => handleDraw(), 150);
+            setTimeout(() => handleDraw(), 10);
             return;
         }
 
@@ -103,7 +124,11 @@ function Game() {
                         {...{ index,  player }}/>
                     ))}
                 </div>
-                <div>Scores Goes Here</div>
+                <div className="mx-auto w-96 text-2xl text-serif">
+                    <p className="text-white mt-5">Next Player: <span>{currentPlayer}</span></p>
+                    <p className="text-white mt-5">Player X wins: <span>{scores["X"]}</span></p>
+                    <p className="text-white mt-5">Player O wins: <span>{scores["O"]}</span></p>
+                </div>
             </div>
         </div>
     );
